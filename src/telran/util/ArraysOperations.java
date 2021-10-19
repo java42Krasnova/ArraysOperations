944aa62ff99ea9fdfadd68062b0bbb52aa0b6031
ikrasnova <i.bas@me.com>
2021-10-19 11:11:57 +0300

    added changes after comments

--- src/telran/util/ArraysOperations.java
+++ src/telran/util/ArraysOperations.java
@@ -37,55 +37,25 @@ public static <T> String remove(T[] array, int index, T[] res) {
 	
 	return OK;
 }
-// V.R. It works, but style isn't good
 public static <T> String insertSorted (T[] sortedArray, T element, T[] res) {	
-	// V.R. This check is done by insert, it is redundant here
-	if (res.length != sortedArray.length+1) {
-		return WRONG_RESULT_LENGTH;
-	}
 	
 	int index = Arrays.binarySearch(sortedArray, element, (Comparator<T>)Comparator.naturalOrder());
-	
-	if (index>=0) {
-		insert(sortedArray, element, index+1, res);
-		return OK;
-	}
-	index = (-1)*(++index);
-	insert(sortedArray, element, index, res);
-		
-	return OK;
-	/* V.R. The following looks much simpler:
 	if(index < 0) {
 		index = -(index+1);
 	}
 	return insert(sortedArray, element, index, res);
-	 */
+
+	
 }
 public static <T> String addLast(T[] array, T element, T[]res) {
-	if (res.length != array.length+1) {
-		return WRONG_RESULT_LENGTH;
-	}
 	
-	//res = Arrays.copyOf(array, array.length+1); * not work why???
-	System.arraycopy(array, 0, res, 0, array.length);
-	res[array.length] = element;
-	
-	return OK;
-	/* V.R. The following looks simpler and shorter. Isn't it?
 	return insert(array, element, array.length, res);
-	 */
+
+	
 }
 public static <T> String addFirst(T[] array, T element, T[]res) {
-	if (res.length != array.length+1) {
-		return WRONG_RESULT_LENGTH;
-	}
-	
-	System.arraycopy(array, 0, res, 1, array.length);
-	res[0]=element;
-	return OK;
-	/* V.R. The following looks simpler and shorter. Isn't it?
 	return insert(array, element, 0, res);
-	 */
+	
 }
 
 }
